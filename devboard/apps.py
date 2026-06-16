59b9abf2d4a9a5a6baf3df20dc6e0954f277746b
helenka <ilona.podlasiak@gmail.com>
2026-06-16 12:39:25 +0200

    first commit w 2gim tygodniu

--- devboard/apps.py
+++ devboard/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class DevboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'devboard'
+    verbose_name = 'API Devboard v.0.1' #tu wpisujemmy przyjzaną nazwe aplikacji w adminie aby bylo ladniej
+
+    def ready(self):
+        print('API Devboard została uruchomiona')   #na starcie wywolujemy komunikat :)
--- devboard/views.py
+++ devboard/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# funkcja indeksowa ktora zwraca w przegladarce odpowiedz (indeksowa jest tylko zaslepka na szybko
+# bo generalnie w html bedziemy pozniej to pisac)
+# def index(request):
+#     return HttpResponse("<h1>DevBoard - etap 1: scaffold!</h1>")
+
+# funkcja renderowa
+def index(request):
+    return render(request, 'index.html')
